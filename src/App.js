77321d83914c5b785bee123697c2972320365f65
umangmalaviya <umang.rutilant@gmail.com>
2023-05-10 09:36:27 +0530

    update - 20230510(1)

--- src/App.js
+++ src/App.js
@@ -8,28 +8,7 @@ function App() {
     <BrowserRouter>
       <Navbar visible={navVisible} show={showNavbar} />
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path='/dashboard' element={
-            <div className={!navVisible ? "page" : "page page-with-navbar"}>
-              <h1>Dashboard</h1>
-            </div>
-          } />
-          <Route path='/analytics' element={
-            <div className={!navVisible ? "page" : "page page-with-navbar"}>
-              <h1>Analystics</h1>
-            </div>
-          } />
-          <Route path='/orders' element={
-            <div className={!navVisible ? "page" : "page page-with-navbar"}>
-              <h1>Orders</h1>
-            </div>
-          } />
-          <Route path='/settings' element={
-            <div className={!navVisible ? "page" : "page page-with-navbar"}>
-              <h1>Settings</h1>
-            </div>
-          } />
-        </Route>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
