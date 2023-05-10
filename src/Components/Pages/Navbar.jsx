69698d453a01f7e2d71ea1256a8ced132a1ec4df
umangmalaviya <umang.rutilant@gmail.com>
2023-05-10 14:21:38 +0530

    sds

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {
-	FaChartBar,
 	FaThLarge,
-	FaShoppingCart,
-	FaCog,
-	FaSignOutAlt
+	FaChalkboardTeacher
 } from 'react-icons/fa';
+import {
+	BsPersonLinesFill,
+	BsCreditCard2BackFill
+} from 'react-icons/bs'
+import { IoIosPaper } from 'react-icons/io'
+import { RiFilePaperFill } from 'react-icons/ri'
+import { SlCalender } from 'react-icons/sl'
+import { TfiDropboxAlt } from 'react-icons/tfi'
+import { TbBeach } from 'react-icons/tb'
+import { VscTable } from 'react-icons/vsc'
+import { MdOutlineExitToApp } from 'react-icons/md'
+import { ImBooks } from 'react-icons/im'
 import { NavLink } from "react-router-dom";
 import "../Style/Navbar.css";
+import Logo from "../Images/logo.svg"
 
 const ICON_SIZE = 20;
 
@@ -15,48 +25,65 @@ function Navbar({ visible, show }) {
 
 	return (
 		<>
-			{/* <div className="mobile-nav">
-				<button
-					className="mobile-nav-btn"
-					onClick={() => show(!visible)}
-				>
-					<FaBars size={24}  />
-				</button>
-			</div> */}
 			{/* <div className='body_content'> */}
 			<nav className={!visible ? 'navbar' : ''}>
 				<button type="button" className="nav-btn" onClick={() => show(!visible)} style={{ right: visible ? "25px" : "0px" }} >
 					{!visible ? ">" : "<"}
 				</button>
 				<div>
-					<NavLink className="logo" to="/" >
-						{/* <img src={require("../assets/Images/logo.png")} alt="logo" /> */}
+					<NavLink className="logo" to="/" style={{ zIndex: visible ? "1" : "-1" }}>
+						<img src={Logo} alt="logo" />
 					</NavLink>
-					<div className="links nav-top">
-						<NavLink to="/dashboard" className="nav-link">
+					<div className="links nav-top" style={{ zIndex: visible ? "1" : "-1" }}>
+						<NavLink to="/demo" className="nav-link" activeclassname='active'>
 							<FaThLarge size={ICON_SIZE} />
 							<span>Dashboard</span>
 						</NavLink>
-						<NavLink to="/analytics" className="nav-link">
-							<FaChartBar size={ICON_SIZE} />
-							<span>Analytics </span>
+						<NavLink to="/demo" className="nav-link" activeclassname='active'>
+							<BsPersonLinesFill size={ICON_SIZE} />
+							<span>Generate ID</span>
 						</NavLink>
-						<NavLink to="/orders" className="nav-link">
-							<FaShoppingCart size={ICON_SIZE} />
-							<span>Orders</span>
+						<NavLink to="#" className="nav-link">
+							<IoIosPaper size={ICON_SIZE} />
+							<span>Exam</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<BsCreditCard2BackFill size={ICON_SIZE} />
+							<span>Fees</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<RiFilePaperFill size={ICON_SIZE} />
+							<span>Result</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<FaChalkboardTeacher size={ICON_SIZE} />
+							<span>Notice Board</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<SlCalender size={ICON_SIZE} />
+							<span>Events</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<TfiDropboxAlt size={ICON_SIZE} />
+							<span>Complain Box</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<TbBeach size={ICON_SIZE} />
+							<span>Holidays</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<VscTable size={ICON_SIZE} />
+							<span>Time Table</span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<ImBooks size={ICON_SIZE} />
+							<span>Class Subject </span>
+						</NavLink>
+						<NavLink to="#" className="nav-link">
+							<MdOutlineExitToApp size={ICON_SIZE} />
+							<span>Log Out</span>
 						</NavLink>
 					</div>
-				</div>
-
-				<div className="links">
-					<NavLink to="/settings" className="nav-link">
-						<FaCog size={ICON_SIZE} />
-						<span>Settings</span>
-					</NavLink>
-					<NavLink to="/Sign-out" className="nav-link">
-						<FaSignOutAlt size={ICON_SIZE} />
-						<span>Logout</span>
-					</NavLink>
 				</div>
 			</nav>
 			{/* </div> */}
